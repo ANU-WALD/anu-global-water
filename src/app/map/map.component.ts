5a4c3b46a7209727b660502eb0f5074738ae6362
Joel Rahman <joel@flowmatters.com.au>
2019-10-12 15:48:25 +1100

    Switch to SCSS for styles

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,7 +6,7 @@ declare let jsdap;
 @Component({
     selector: 'app-map',
     templateUrl: './map.component.html',
-    styleUrls: ['./map.component.css']
+    styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
 
