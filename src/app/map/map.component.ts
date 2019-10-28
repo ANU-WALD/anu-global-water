58e951fde89006a74bb72f3eb4e9947018c9e616
Ali Ershadi <ali.ershadi2@gmail.com>
2019-10-28 11:21:10 +1000

    new push to test after system move - 2

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -21,6 +21,7 @@ export class MapComponent implements OnInit {
     this.pointData.getLayers().subscribe(layers => console.log(layers));
   }
 
+  public test = null;
   public DAT = {
     mapbox_token: 'pk.eyJ1IjoiZmFybWluZ2RzcyIsImEiOiJhNDVhOWY2MGIxMjgwYjI5OTdiOGRhMTM1NGE1YTFkYyJ9.cDFYFuz0wEbd0rxM-6djsw',
     mapbox_baseLayers: {},
